aa5a3753577860a974ccb2f76cec347da8597295
koltrast <koltrast@pm.me>
2023-02-03 00:58:21 +0100

    optimize code

--- main.py
+++ main.py
@@ -4,30 +4,48 @@
 import sys
 import pandas as pd
 
-# global variables
-a = 0
-tranche = None
-item = []
-obj = []
-price = []
-logo = ('''
- __   __  _______ 
-|  | |  ||    _  |
-|  |_|  ||   |_| |
-|       ||    ___| h
-|_______||___|2023
-ŒUVRIR
-''')
-
 # dataframe
 data = pd.DataFrame({
     "A": [25, 30, 35, 40, 50, 60, 75, 90, 110],
     "B": [30, 35, 40, 45, 55, 65, 80, 95, 115],
     "C": [10, 15, 20, 25, 35, 45, 55, 70, 90]})
 
-# functions
-def ticket():
+# exec
+logo = ('''
+ __   __  _______ 
+|  | |  ||    _  |
+|  |_|  ||   |_| |
+|       ||    ___|
+|_______||___|2023
+ŒUVRIR
+''')
+
+obj = []
+price = []
+
+while True:
+	tranche = None
+	item = []
+	while True:
+		value = input("Enter value \n")
+		while not value.isdigit():
+		    item.append(value)
+		    value = input("Enter value \n")
+
+		tranche = int(value)
+		break
+
+	for i in range(len(item)):
+		price.append(data.at[tranche, item[i]])
+		if item[i] == "A":
+			obj.append("veste")
+		elif item[i] == "B":
+			obj.append("combinaison")
+		elif item[i] == "C":
+			obj.append("casquette")
+
 	with open("ticket.tmp", "w", encoding="utf-8") as f:
+		import os
 		f.write(f"{logo}\n\n")
 		if tranche <= 1:
 			f.write(f"Vous avez déclaré toucher un revenu\ninférieur au 1er décile.\n")
@@ -35,52 +53,15 @@ def ticket():
 			f.write(f"Vous avez déclaré toucher un revenu\ncompris entre le {tranche}e et le {tranche+1}e décile.\n")
 		elif tranche == 9:
 			f.write(f"Vous avez déclaré toucher un revenu\nsupérieur au 9e décile.\n")
-		f.write(f"\nAfin de garantir une equité tarifaire\n")
+		f.write(f"\nAfin de garantir une équité tarifaire\n")
 		if len(item) > 1:
 			f.write(f"les prix ont été ajustés.\n\n")
 		else:
-			f.write(f"le prix à été ajusté.\n\n")
+			f.write(f"le prix a été ajusté.\n\n")
 		for i in range(len(obj)):
 			f.write(f"- {obj[i]} {price[i]}€\n")
-		f.write(f"\nTOTAL :		                {sum(price)}€")
-
-
-def to_printer_cli():
-	import os
-	os.system("lpr -P EPSON_TM-T20III ticket.tmp")
-	print("done")
-
-def to_printer_sel():
-	with open("ticket.tmp", "a", encoding="utf-8") as f:
-		f.write("\n\nexemplaire UP")
-		import os
-		os.system("lpr -P EPSON_TM-T20III ticket-sel.tmp")
-
-# exec
-while a == 0:
-	value = input("Enter value \n")
-	if value.isdigit():
-		tranche = int(value)
-		if not item:
-			a = 0
-		else:
-			a = 1
-	else:
-		item.append(value)
-		if tranche is None:
-			a = 0
-		else:
-			a = 1
-
-for i in range(len(item)):
-	price.append(data.at[tranche, item[i]])
-	if item[i] == "A":
-		obj.append("veste                     ")
-	elif item[i] == "B":
-		obj.append("combinaison               ")
-	elif item[i] == "C":
-		obj.append("casquette                 ")
-
-ticket()
-to_printer_cli()
-to_printer_sel()
+		f.write(f"\n TOTAL : {sum(price)}€")
+		f.write(f"\n\nTickets imprimé en 2 exemplaires")
+		os.system("lpr -P EPSON_TM-T20III ticket.tmp")
+		f.write(f"\n\nexemplaire UP")
+		os.system("lpr -P EPSON_TM-T20III ticket.tmp")
