f9fb44a98df05fe96c71578f98b63bf24a1dffbe
koltrast <koltrast@pm.me>
2024-09-26 00:25:04 +0200

    set the hero
    set image to the right size
    fix the image display
    structure the code for int-step1

--- public/js/main.js
+++ public/js/main.js
@@ -6,7 +6,7 @@ const enteredItemsList = document.getElementById("entered-items-list");
 const decileSelection = document.getElementById("decile-selection");
 const shoppingValidation = document.getElementById("shopping-validation");
 const validateButton = document.getElementById("validate-ticket");
-const itemImage = document.getElementById("item-image");
+const itemImage = document.getElementById("int-step1");
 const articleNames = {
 	"UP-VST-23": "Veste",
 	"UP-CMB-23": "Combinaison",
